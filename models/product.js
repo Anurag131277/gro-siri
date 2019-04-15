8f9f8d1b5fd92c604882360ac5b08474cf882e24
Anurag131277 <anurag1887@gmail.com>
2019-04-16 02:46:18 +0530

    Logic added for using SqL

--- models/product.js
+++ models/product.js
@@ -8,14 +8,21 @@ module.exports = class Item{
 
 	constructor(id,name,imageUrl,dop,expire_after){
 		this.item_id=id;
-		this.name = name;
+		this.itemName = name;
 		this.imageUrl = imageUrl;
 		this.date_of_purchase = dop;
 		this.expire_after = expire_after;
 	}
 
 	save(){ //function/method
-		
+		if(this.id){
+			console.log('Inside Save Update data');
+			db.execute('')
+		}else{
+			return db.execute('INSERT INTO items (name,imageurl,date_of_purchase,expire_after) VALUES (?,?,?,?)',
+			[this.itemName,this.imageUrl,this.date_of_purchase,this.expire_after]
+			);
+		}
 		//products.push(this);
 	}
 
@@ -29,7 +36,7 @@ module.exports = class Item{
 
 	//use promise and not callbacks
 	static findById(id){ 
-		return db.execute('SELECT * from items WHERE item_id='+id);
+		return db.execute('SELECT * from items WHERE item_id = ?',[id]);
 	}
 
 
